c9e342e461052898f79118204173067717a7403a
Alireza Omidi <alirezaomidi@users.noreply.github.com>
2024-12-04 14:18:50 -0800

    Update multi-gpu.sh

--- slurm/multi-gpu.sh
+++ slurm/multi-gpu.sh
@@ -2,19 +2,19 @@
 sbatch <<EOT
 #!/bin/bash
 #SBATCH --job-name=colabfold
-#SBATCH --account=def-gsponer
+#SBATCH --account=def-gsponer  # or rrg-gsponer
 #SBATCH --time=2-23:58:00
 #SBATCH --nodes=1
 #SBATCH --array=0-1
 #SBATCH --gpus-per-node=p100:4
 #SBATCH --cpus-per-task=24
-#SBATCH --mem=120000
+#SBATCH --mem=120G
 #SBATCH --output=outputs/%A.%a.out
 #SBATCH --mail-user=CWL@student.ubc.ca # adjust this to match your email address
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 
-module load gcc/9.3.0 openmpi/4.0.3 cuda/11.4 cudnn/8.2.0 kalign/2.03 hmmer/3.2.1 openmm-alphafold/7.5.1 hh-suite/3.3.0 python/3.8 mmseqs2
+module load StdEnv/2020 gcc/9.3.0 openmpi/4.0.3 cuda/11.4 cudnn/8.2.0 kalign/2.03 hmmer/3.2.1 openmm-alphafold/7.5.1 hh-suite/3.3.0 python/3.8 mmseqs2
 
 source ~/alphafold_env/bin/activate
 
@@ -29,8 +29,7 @@ END=\$((\$BEGIN + 3))
 
 echo "$@"
 srun parallel -j4 CUDA_VISIBLE_DEVICES='{=1 \$_=\$arg[1]%4 =}' python -u batch.py \
-     --num-recycle 20 \
-     --recycle-early-stop-tolerance 0.5 \
+     --num-recycle 3 \
      --model-type alphafold2_multimer_v2 \
      --num-seeds 1 \
      --zip \
